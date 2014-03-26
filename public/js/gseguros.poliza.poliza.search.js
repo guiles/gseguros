5f3f513f9b092c3d24b8a3fc652ac34754907d47
Guiles <guillermo.caserotto@gmail.com>
2014-03-26 09:20:08 -0300

    Inicio repositorio

---
+++ public/js/gseguros.poliza.poliza.search.js
@@ -0,0 +1,115 @@
+$(document).ready(
+		function() {
+			
+			$('#fecha_pedido').datepicker(
+					{
+						dateFormat : 'yy-mm-dd',
+						dayNamesMin : [ 'Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi',
+								'Sa' ],
+						monthNames : [ 'Enero', 'Febrero', 'Marzo', 'Abril',
+								'Mayo', 'Junio', 'Julio', 'Agosto',
+								'Septiembre', 'Octubre', 'Noviembre',
+								'Diciembre' ]
+					});
+
+			$('#fecha_vigencia').datepicker(
+					{
+						dateFormat : 'yy-mm-dd',
+						dayNamesMin : [ 'Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi',
+								'Sa' ],
+						monthNames : [ 'Enero', 'Febrero', 'Marzo', 'Abril',
+								'Mayo', 'Junio', 'Julio', 'Agosto',
+								'Septiembre', 'Octubre', 'Noviembre',
+								'Diciembre' ]
+					});
+
+			$('#fecha_vigencia_hasta').datepicker(
+					{
+						dateFormat : 'yy-mm-dd',
+						dayNamesMin : [ 'Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi',
+								'Sa' ],
+						monthNames : [ 'Enero', 'Febrero', 'Marzo', 'Abril',
+								'Mayo', 'Junio', 'Julio', 'Agosto',
+								'Septiembre', 'Octubre', 'Noviembre',
+								'Diciembre' ]
+					});
+
+
+
+			
+			// Confirmo busqueda de polizas
+			$('#confirm_search_poliza').click(function() {
+				
+								// trae id de tab
+								var tabs_sel = $('#tabs').tabs();
+								var idx = tabs_sel.tabs('option',
+										'selected');
+
+								// Trae el tab correspondiente
+								var tab = $('#tabs ul li a')[idx];
+								// //console.debug($('#tabs ul li a'));
+								var href = $(tab).attr('href');
+
+								// suponemos que el form es valido
+
+								var data = $('#form_search_poliza').serializeArray();
+
+
+								$.ajax({
+											url : "./poliza/poliza/search",
+											data : data,
+											success : function(result) {
+												$(href).html(result);
+											}
+										});
+
+							});
+			
+				$('#tab_search_poliza').click(function() {
+				
+					// trae id de tab
+					var tabs_sel = $('#tabs').tabs();
+					var idx = tabs_sel.tabs('option',
+							'selected');
+	
+					// Trae el tab correspondiente
+					var tab = $('#tabs ul li a')[idx];
+					// //console.debug($('#tabs ul li a'));
+					var href = $(tab).attr('href');
+	
+					$.ajax({
+								url : "./poliza/poliza/search",
+								success : function(result) {
+									$(href).html(result);
+								}
+							});
+
+				});
+
+			
+				/*
+				 * 
+				 */
+			$('#imprimir_search_poliza').click(function() {
+					
+			var DocumentContainer = document.getElementById('div_table_poliza_search');
+					
+				var html = '<html><head>'+
+	               '<link href="css/template.css" rel="stylesheet" type="text/css" />'+
+	               '</head><body style="background:#ffffff;">'+
+	               DocumentContainer.innerHTML+
+	               '</body></html>';
+//var html =  DocumentContainer.innerHTML;
+	    var WindowObject = window.open("", "PrintWindow",
+	    	    "width=750,height=650,top=50,left=50,toolbars=no,scrollbars=yes,status=no,resizable=yes");
+	    WindowObject.document.writeln(html);
+	    WindowObject.document.close();
+	    WindowObject.focus();
+	    WindowObject.print();
+	    WindowObject.close();
+	    document.getElementById('div_table_poliza_search').style.display='block';
+	
+		});
+				
+		});
+
